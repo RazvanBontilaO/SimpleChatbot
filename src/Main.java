6151cd6b3f471382ba4883c7a2a9e60b3a427e03
RazvanBontilaO <razvan.bontila@student.upt.ro>
2025-10-16 16:15:27 +0300

    Stage four implemented: added programming knowledge quiz functionality

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,15 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         Scanner scanner = new Scanner(System.in);
+        boolean quizz = true;
 
         System.out.println("Hello! My name is Aid" +
                 "\nI was created in 2025. " +
-                "\nPlease, remind me your name\n");
+                "\nPlease, remind me your name");
 
         String name = scanner.nextLine();
         System.out.println("What a great name you have, " + name + "!");
+        System.out.println();
 
         System.out.println("Let me guess your age." +
                 "\nEnter remainders of dividing your age by 3, 5 and 7");
@@ -22,12 +24,33 @@ public class Main {
         int age = (r1 * 70 + r2 * 21 + r3 * 15) % 105;
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
 
+        System.out.println();
         System.out.println("Now I will prove to you that I can count to any number you want. " +
                 "\nPlease, enter a number:");
         int num = scanner.nextInt();
         for (int i = 1; i <= num; i++) {
-            System.out.print(i + "... ");
+            System.out.print(i + " ");
             sleep(1000,1);
+        }
+        System.out.println();
+        System.out.println("Let's test your programming knowledge.");
+        while (quizz) {
+            System.out.println("Why do we use methods?");
+            System.out.println("""
+                    1. To repeat a statement multiple times.\
+                    
+                    2. To decompose a program into several small subroutines.\
+                    
+                    3. To determine the execution time of a program.\
+                    
+                    4. To interrupt the execution of a program.""");
+            int answer = scanner.nextInt();
+            if (answer == 2) {
+                System.out.println("Completed, have a nice day!");
+                quizz = false;
+            } else {
+                System.out.println("Incorrect answer. Please, try again.");
+            }
         }
     }
 }
